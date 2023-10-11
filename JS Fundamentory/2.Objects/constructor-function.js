103366873264d3e2ab9c37758a7b47a22662a518
Quyết Nguyễn Hữu <huuquyet498@gmail.com>
2023-10-11 23:32:47 +0700

    OOP in Javascript

--- JS Fundamentory/2.Objects/constructor-function.js
+++ JS Fundamentory/2.Objects/constructor-function.js
@@ -18,8 +18,9 @@ console.log(user1.isAdmin); // false
 /**
 - When a function is executed with new, it does the following steps:
 
-    1. A new empty object is created and assigned to this.
-    2. The function body executes. Usually it modifies this, adds new properties to it.
+    1. A new empty object is created
+    2. Function is called, this = {}
+    2. The function body executes. Usually it modifies this, adds new properties to it. Linked to prototype
     3. The value of this is returned.
 
 In other words, new User(...) does something like:
---
+++ OOP/theory.js
@@ -0,0 +1,28 @@
+// Object-Oriented Programming is a programming style based on classes and objects. These group data (properties) and methods (actions) inside a box.
+
+// OOP was developed to make code more flexible and easier to maintain.
+
+// What is the structure of object-oriented programming?
+//  -   Classes are user-defined data types that act as the blueprint for individual objects, attributes and methods.
+//  -   Objects are instances of a class created with specifically defined data. Objects can correspond to real-world objects or an abstract entity. When class is defined initially, the description is the only object that is defined.
+//  -   Methods are functions that are defined inside a class that describe the behaviors of an object. Each method contained in class definitions starts with a reference to an instance object. Additionally, the subroutines contained in an object are called instance methods. Programmers use methods for reusability or keeping functionality encapsulated inside one object at a time.
+//  -   Properties are defined in the class template and represent the state of an object. Objects will have data stored in the Properties field. Class properties belong to the class itself.
+
+// JavaScript is prototype-based procedural language, which means it supports both functional and object-oriented programming.
+// There are 4 main principle in OOP, and they are:
+//  1. Abstraction
+//  2. Encapsulation
+//  3. Inheritance
+//  4. Polymorphism
+
+//  ----------------ABSTRACTION--------------------
+// Abstraction means hiding detail logic that do not matter to the user and only showing essential features or functions
+
+//  ----------------ENCAPSULATION------------------
+// Encapsulation means keeping properties and methods private inside a class and only select information is exposed, so that they are not accessible from outside that class. This makes the program secure and avoids manipulation of unintended data
+
+//  ----------------INHERITANCE--------------------
+// Inheritance makes all properties and methods available to instances. Classes can reuse code from other classes. Relationships and subclasses between objects can be assigned, enabling developers to reuse common logic while still maintaining a unique hierarchy. This property of OOP forces a more thought data analysis, reduces development time and ensures a high level of accuracy.
+
+//  ----------------POLYMORPHISM--------------------
+// Objects are designed to share behaviors and they can take on more than one form. The program will determine which meaning or usage is necessary for each execution of that object from a parent class, reducing the need to duplicate code. A child class is then created, which extends the functionality of the parent class. Polymorphism allows different types of objects to pass through the same interface.
